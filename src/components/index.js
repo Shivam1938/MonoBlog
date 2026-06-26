7a7fd05cfc2e23ecca7423038eeaa7b994416d2b
Shivam1938 <boymax1938@gmail.com>
2026-06-27 00:39:38 +0530

    Added Some Code with Header and Footer Code

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,15 @@
 import Header from './Header/Header.jsx'
 import Footer from './Footer/Footer.jsx'
+import Container from "../components/container/Container.jsx"
+import Logo from "./Logo.jsx"
+import LogoutBtn from "./Header/LogoutBtn.jsx"
 
-export { Header, Footer }
+
+export { 
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+    
+}
